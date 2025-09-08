75790d91b34997b704297934aa21fd253968f341
David Johnston <david@blacksheepcode.com>
2025-09-08 21:40:42 +1000

    Add another test

--- src/lib/Autocomplete_A/Autocomplete.test.tsx
+++ src/lib/Autocomplete_A/Autocomplete.test.tsx
@@ -5,7 +5,7 @@ import { userEvent } from "@storybook/test";
 
 describe(Autocomplete, () => {
     it("Displays the values when the input is focused, has text, and there are results", async () => {
-        render(<Autocomplete
+        const renderResult = render(<Autocomplete
             searchValue="123"
             onChangeSearchValue={vi.fn()}
             selectedValue={null}
@@ -16,6 +16,7 @@ describe(Autocomplete, () => {
 
         />)
 
+
         expect(screen.getByRole("textbox")).toBeInTheDocument();
         userEvent.click(screen.getByRole("textbox"));
 
@@ -24,7 +25,9 @@ describe(Autocomplete, () => {
             expect(screen.getByText("Foo")).toBeInTheDocument();
             expect(screen.getByText("Bar")).toBeInTheDocument();
         })
-    })
+    });
+
+
     it("Displays the no results message when the input is focused, has text, and there are not results", async () => {
         render(<Autocomplete
             searchValue="123"
@@ -43,5 +46,40 @@ describe(Autocomplete, () => {
         await waitFor(() => {
             expect(screen.getByText("No results found")).toBeInTheDocument();
         })
+    })
+
+    it("Is first loading, and then is not loading", async () => {
+        // First the component is loading
+        const renderResult = render(<Autocomplete
+            searchValue="123"
+            onChangeSearchValue={vi.fn()}
+            selectedValue={null}
+            onSelectValue={vi.fn()}
+            renderItem={(v) => <div>{v.name}</div>}
+            isLoading={true}
+            availableOptions={[] as Array<{ id: string, name: string }>}
+
+        />)
+
+        expect(screen.getByRole("textbox")).toBeInTheDocument();
+        userEvent.click(screen.getByRole("textbox"));
+
+        await waitFor(() => {
+            expect(screen.getByText("Loading...")).toBeInTheDocument();
+        });
+
+        // And then it has result
+        renderResult.rerender(<Autocomplete
+            searchValue="123"
+            onChangeSearchValue={vi.fn()}
+            selectedValue={null}
+            onSelectValue={vi.fn()}
+            renderItem={(v) => <div>{v.name}</div>}
+            isLoading={false}
+            availableOptions={[{ id: "1", name: "Foo" }, { id: "2", name: "Bar" }]}
+        />)
+
+        expect(screen.getByText("Foo")).toBeInTheDocument();
+        expect(screen.getByText("Bar")).toBeInTheDocument();
     })
 })
